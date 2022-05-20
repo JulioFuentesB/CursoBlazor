036343ef6a591c0df143d2c4c47e55ea0f2e8567
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-05-20 09:00:49 -0500

    Seretoma curzo blazor

---
+++ BlazorPeliculas/Client/Helpers/UtilidadesString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public class UtilidadesString
+    {
+        //exprecion implicita, no sabe donde empieza y donde terman
+        public static string EnMayiscular(string valor) => valor.ToUpper();
+    }
+}
