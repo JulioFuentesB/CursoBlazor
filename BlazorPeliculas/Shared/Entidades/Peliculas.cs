999d89ae196af2134fbc614cd6d6a81029a6b230
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-05-23 11:09:03 -0500

    Video 13 bucles de una clase peliculas

--- BlazorPeliculas/Shared/Entidades/Peliculas.cs
+++ BlazorPeliculas/Shared/Entidades/Peliculas.cs
@@ -10,6 +10,7 @@ namespace BlazorPeliculas.Shared.Entidades
     {
 
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string Titulo { get; set; }
+        public DateTime FechaLanzamiento { get; set; }
     }
 }
