2f388e655ca456d39084bf5e93032274de49d5c8
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-05-31 14:27:56 -0500

    Video 31 invocar metodos de javascript desde c#

---
+++ BlazorPeliculas/Client/Helpers/IJSRuntimeExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Helpers
+{
+    public static class IJSRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm( this IJSRuntime js,string mensaje)
+        {
+            await js.InvokeVoidAsync("console.log", "primer parametro console log");
+
+            return await js.InvokeAsync<bool>("confirm", mensaje);
+        }
+
+    }
+}
