798efb0cbe625f8749fec3070517e3f5a72b8494
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-05-31 14:01:33 -0500

    Video  28 Servicios con interfaces

---
+++ BlazorPeliculas/Client/Repositorios/Repositorio.cs
@@ -0,0 +1,21 @@
+﻿using BlazorPeliculas.Shared.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorPeliculas.Client.Repositorios
+{
+    public class Repositorio : IRepositorio
+    {
+        public List<Peliculas> ObtenrPeliculas()
+        {
+            return new List<Peliculas>()
+                {
+                    new Peliculas(){Titulo = "Spider-Man: Far From Home", FechaLanzamiento  = new DateTime(2019, 7, 2)},
+                    new Peliculas(){Titulo = "Moana", FechaLanzamiento  = new DateTime(2016, 11, 23)},
+                    new Peliculas(){Titulo = "Inception", FechaLanzamiento  = new DateTime(2010, 7, 16)}
+                };
+        }
+    }
+}
