0d3a924b7b738e41b746431595a021a35b19930d
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-06-09 18:24:53 -0500

    Video  52 creando el primer formulario

---
+++ BlazorPeliculas/Shared/Entidades/GenerosPeliculas.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlazorPeliculas.Shared.Entidades
+{
+    public class GenerosPeliculas
+    {
+        public int PeliculaId { get; set; }
+        public int GeneroId { get; set; }
+    }
+}
