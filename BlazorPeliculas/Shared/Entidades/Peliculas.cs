a4c4e358ec00335edcc7086eefd0aa2e6e36df9d
Julio Fuentes B <76795396+JulioFuentesB@users.noreply.github.com>
2022-06-13 08:32:46 -0500

    Video 60 componente de seleccion multiple funcional

--- BlazorPeliculas/Shared/Entidades/Peliculas.cs
+++ BlazorPeliculas/Shared/Entidades/Peliculas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,16 +9,20 @@ namespace BlazorPeliculas.Shared.Entidades
 {
     public class Peliculas
     {
-
         public int Id { get; set; }
+        [Required]
         public string Titulo { get; set; }
-        public DateTime FechaLanzamiento { get; set; }
+        public string Resumen { get; set; }
+        public bool EnCartelera { get; set; }
+        public string Trailer { get; set; }
+        [Required]
+        public DateTime? FechaLanzamiento { get; set; }
         public string Poster { get; set; }
+        public List<GenerosPeliculas> GenerosPelicula { get; set; } = new List<GenerosPeliculas>();
         public string TituloCortado
         {
             get
             {
-
                 if (string.IsNullOrWhiteSpace(Titulo))
                 {
                     return null;
